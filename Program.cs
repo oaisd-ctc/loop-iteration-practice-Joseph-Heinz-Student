00c2d2a7e2971ea5a253b64b696cbb5059c86fa4
Joseph Heinz <joheb41@student.oaisd.org>
2024-10-18 14:43:34 +0000

    started

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteNThrough1_For(5);
+    }
+
+    public static void Write1ThroughN_While(int n)
+    {
+        int current = 1;
+        if (n < 1)
+        {
+            OutOfRangeMessage(n);
+            return;
+        }
+        while (current <= n)
+        {
+            Console.WriteLine(current);
+            current++;
+        }
+    }
+
+    public static void Write1ThroughN_For(int n)
+    {
+        if (n < 1)
+        {
+            OutOfRangeMessage(n);
+            return;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public static void WriteNThrough1_While(int n)
+    {
+        if (n < 1)
+        {
+            OutOfRangeMessage(n);
+            return;
+        }
+        while (n >= 1)
+        {
+            Console.WriteLine(n);
+            n--;
+        }
+    }
+
+    public static void WriteNThrough1_For(int n)
+    {
+        if (n < 1)
+        {
+            OutOfRangeMessage(n);
+            return;
+        }
+        for (int i = n; i >= n; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public static void OutOfRangeMessage(int n)
+    {
+        Console.WriteLine($"{n} is out of range.");
+    }
+}
